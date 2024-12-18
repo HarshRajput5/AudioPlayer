b0796d32ac617e69717d7ef9fe62992f87a38568
hplio <harshilpatel9724@gmail.com>
2024-12-18 19:52:18 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -31,71 +31,70 @@ class MusicScreen extends StatelessWidget {
           style: Theme.of(context).textTheme.headlineMedium,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 18,
-            vertical: 12,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.my_library_music_sharp,size: 200,),
-              // controller.customSlider(),
-              // Obx(() => controller.GuestSelectionSlider()),
-              Obx(
-                () => Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          controller.player.seekToPrevious();
-                          controller.countPrevius.value++;
-                          print(
-                              "previus count ${controller.countPrevius.value}");
-                        },
-                        icon: Icon(Icons.arrow_back_ios)),
-                    IconButton(
-                        onPressed: () {
-                          // if(controller.isPlay.value){
-                          //   controller.player.pause();
-                          // }
-                          // else{
-                          //   controller.player.play();
-                          // }
-                          controller.toggelplay();
-                          controller.countPlay.value++;
-                          print("Play count ${controller.countPlay.value}");
-                        },
-                        icon: Icon(controller.isPlay.value
-                            ? Icons.pause
-                            : Icons.play_arrow)),
-                    IconButton(
-                        onPressed: () {
-                          controller.player.seekToNext();
-                          controller.countNext.value++;
-                          print("Next count ${controller.countNext.value}");
-                        },
-                        icon: Icon(Icons.arrow_forward_ios))
-                  ],
-                ),
-              ),
-
-              Row(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 18,
+          vertical: 12,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .2,
+            ),
+            Icon(
+              Icons.my_library_music_sharp,
+              size: 200,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Obx(
+              () => Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(controller.player.currentIndex.toString()),
-                  Text(controller.player.duration.toString())
+                  IconButton(
+                      onPressed: () {
+                        controller.player.seekToPrevious();
+                        controller.countPrevius.value++;
+                        print("previus count ${controller.countPrevius.value}");
+                      },
+                      icon: Icon(Icons.arrow_back_ios,size: 33,)),
+                  IconButton(
+                      onPressed: () {
+                        // if(controller.isPlay.value){
+                        //   controller.player.pause();
+                        // }
+                        // else{
+                        //   controller.player.play();
+                        // }
+                        controller.toggelplay();
+                        controller.countPlay.value++;
+                        print("Play count ${controller.countPlay.value}");
+                      },
+                      icon: Icon(controller.isPlay.value
+                          ? Icons.pause
+                          : Icons.play_arrow,size: 33)),
+                  IconButton(
+                      onPressed: () {
+                        controller.player.seekToNext();
+                        controller.countNext.value++;
+                        print("Next count ${controller.countNext.value}");
+                      },
+                      icon: Icon(Icons.arrow_forward_ios,size: 33))
                 ],
               ),
-              // Obx(
-              //   ()=> ElevatedButton(
-              //     onPressed: ()=>controller.toggelplay(),
-              //     child: Text(controller.isPlay.value ? 'Pause' : 'Play'),
-              //   ),
-              // ),
-            ],
-          ),
+            ),
+
+            // Obx(
+            //   ()=> ElevatedButton(
+            //     onPressed: ()=>controller.toggelplay(),
+            //     child: Text(controller.isPlay.value ? 'Pause' : 'Play'),
+            //   ),
+            // ),
+          ],
         ),
       ),
     );
